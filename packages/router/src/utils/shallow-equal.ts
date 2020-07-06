841d4ae8bd4c4ea826e4dc6d6d7d481cb3b67bce
George Payne <georgepayne@hotmail.com>
2020-07-06 17:57:58 +0200

    add ci - set up github ci - add lint command to packages - apply prettier to missed files

--- packages/router/src/utils/shallow-equal.ts
+++ packages/router/src/utils/shallow-equal.ts
@@ -19,17 +19,17 @@ const hasOwnProperty = Object.prototype.hasOwnProperty;
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/is
  */
 const is = (x: any, y: any) => {
-  // SameValue algorithm
-  if (x === y) {
-    // Steps 1-5, 7-10
-    // Steps 6.b-6.e: +0 != -0
-    // Added the nonzero y check to make Flow happy, but it is redundant
-    return x !== 0 || y !== 0 || 1 / x === 1 / y;
-  } else {
-    // Step 6.a: NaN == NaN
-    return x !== x && y !== y;
-  }
-}
+    // SameValue algorithm
+    if (x === y) {
+        // Steps 1-5, 7-10
+        // Steps 6.b-6.e: +0 != -0
+        // Added the nonzero y check to make Flow happy, but it is redundant
+        return x !== 0 || y !== 0 || 1 / x === 1 / y;
+    } else {
+        // Step 6.a: NaN == NaN
+        return x !== x && y !== y;
+    }
+};
 
 /**
  * Performs equality by iterating through keys on an object and returning false
@@ -37,27 +37,35 @@ const is = (x: any, y: any) => {
  * Returns true when the values of all keys are strictly equal.
  */
 export const shallowEqual = (objA: any, objB: any) => {
-  if (is(objA, objB)) {
-    return true;
-  }
-
-  if (typeof objA !== 'object' || objA === null || typeof objB !== 'object' || objB === null) {
-    return false;
-  }
-
-  var keysA = Object.keys(objA);
-  var keysB = Object.keys(objB);
-
-  if (keysA.length !== keysB.length) {
-    return false;
-  }
-
-  // Test for A's keys different from B.
-  for (var i = 0; i < keysA.length; i++) {
-    if (!hasOwnProperty.call(objB, keysA[i]) || !is(objA[keysA[i]], objB[keysA[i]])) {
-      return false;
+    if (is(objA, objB)) {
+        return true;
     }
-  }
 
-  return true;
-}
+    if (
+        typeof objA !== 'object' ||
+        objA === null ||
+        typeof objB !== 'object' ||
+        objB === null
+    ) {
+        return false;
+    }
+
+    var keysA = Object.keys(objA);
+    var keysB = Object.keys(objB);
+
+    if (keysA.length !== keysB.length) {
+        return false;
+    }
+
+    // Test for A's keys different from B.
+    for (var i = 0; i < keysA.length; i++) {
+        if (
+            !hasOwnProperty.call(objB, keysA[i]) ||
+            !is(objA[keysA[i]], objB[keysA[i]])
+        ) {
+            return false;
+        }
+    }
+
+    return true;
+};
