d5dc0abfe981e3e2c364a2f28f3fd37037deecce
George Payne <georgepayne@hotmail.com>
2020-07-08 16:24:17 +0200

    es-icon - clear out cloud icon set - integrate include-icon with es-icon directly - modify icons.json scheme

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -2,3 +2,4 @@ export * from './components';
 export { AccordianSection } from './components/es-accordian/types';
 export { TableColumn } from './components/es-table/types';
 export { toast } from './utils/toast';
+export { iconStore } from './utils/iconStore';
