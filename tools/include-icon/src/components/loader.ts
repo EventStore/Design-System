0afc7111619e008452999154fc7477a9ad42fcad
George Payne <georgepayne@hotmail.com>
2020-07-09 11:20:53 +0200

    clean up include-icon based on usage in cloud

--- tools/include-icon/src/components/loader.ts
+++ tools/include-icon/src/components/loader.ts
@@ -4,13 +4,15 @@ export const convertToLoader = (indexMap: IndexMap) => `
 import { iconStore } from '@eventstore/components';
 
 iconStore.addIcons({
-    ${Array.from(
-        indexMap,
-        ([name, { path, component }]) =>
-            `get '${name}'(){return import('${path.replace(
-                '.tsx',
-                '',
-            )}').then((module) => module.${component})}`,
-    )}
+    ${Array.from(indexMap)
+        .sort(([a], [b]) => (a < b ? -1 : a > b ? 1 : 0))
+        .map(
+            ([name, { path, component }]) =>
+                `get '${name}'(){return import('${path.replace(
+                    '.tsx',
+                    '',
+                )}').then((module) => module.${component})}`,
+        )
+        .join(',')}
 });
 `;
