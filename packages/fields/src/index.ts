0220a1df5c8da4287979c3c8d2aefefce462f490
George Payne <georgepayne@hotmail.com>
2020-07-29 13:48:25 +0200

    add mask option to es-input - add imask dependancy - hook up mask options - export Field - bump deps for external lib import bug

--- packages/fields/src/index.ts
+++ packages/fields/src/index.ts
@@ -1,3 +1,6 @@
 export type { Components, JSX } from './components';
 export type { FieldChangeEvent, Severity, ValidationMessages } from './types';
+export type { MaskOptions } from './components/es-input/es-input';
+
 export { createWorkingData } from './utils/workingData/createWorkingData';
+export { Field } from './components/Field/Field';
