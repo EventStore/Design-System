681a7132bf59da6947bb87c03faa6710260e3548
George Payne <georgepayne@hotmail.com>
2020-08-19 14:34:23 +0200

    Warn on non-matching breadcrumb - if the breadcrumb doesnt match the route, log a warning to the console - add option to disable it - add logger to @eventstore/components

---
+++ packages/components/src/utils/logger.ts
@@ -0,0 +1,6 @@
+import { createLogger } from '@eventstore/utils';
+
+export const logger = createLogger(
+    '@eventstore/components',
+    'radial-gradient( circle farthest-corner at 10% 20%,  rgba(0,95,104,1) 0%, rgba(15,156,168,1) 90% )',
+);
