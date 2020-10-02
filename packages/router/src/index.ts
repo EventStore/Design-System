616611aae08d0ca87a29a719a1f92b0ef042f6ef
George Payne <georgepayne@hotmail.com>
2020-10-02 16:22:22 +0200

    [router] ConditionalRedirect & fillUrl - add fillUrl helper to public router - add ConditionalRedirect component - some low-hanging eslint warning fixes

--- packages/router/src/index.ts
+++ packages/router/src/index.ts
@@ -1,8 +1,9 @@
-export { default as Link } from './components/Link';
-export { default as PageTitle } from './components/PageTitle';
-export { default as Redirect } from './components/Redirect';
-export { default as Route } from './components/Route';
-export { default as Switch } from './components/Switch';
+export { ConditionalRedirect } from './components/ConditionalRedirect';
+export { Link } from './components/Link';
+export { PageTitle } from './components/PageTitle';
+export { Redirect } from './components/Redirect';
+export { Route } from './components/Route';
+export { Switch } from './components/Switch';
 
 export * from './types';
-export { default as router } from './utils/publicRouter';
+export { router } from './utils/publicRouter';
