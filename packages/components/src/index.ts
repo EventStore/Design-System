8279043ad97293ef6a6eb6c6b72ee215c2eac08b
George Payne <georgepayne@hotmail.com>
2021-01-19 11:57:42 +0100

    export WizardPage from root

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,5 +1,6 @@
 export type { Components, JSX } from './components';
 export type { AccordianSection } from './components/es-accordian/types';
 export type { TableCells, TableCell } from './components/es-table/types';
+export type { WizardPage } from './components/es-wizard/types';
 export { toast } from './utils/toast';
 export { iconStore } from './utils/iconStore';
