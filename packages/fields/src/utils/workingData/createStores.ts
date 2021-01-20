31c9d28e4bd78ad9b6c69565f824a9d6dcd9b431
George Payne <georgepayne@hotmail.com>
2021-01-20 18:59:27 +0100

    Allow workingData to be nested

--- packages/fields/src/utils/workingData/createStores.ts
+++ packages/fields/src/utils/workingData/createStores.ts
@@ -6,7 +6,9 @@ import {
     ValidationMessages,
     ValidationFailedCallback,
     BeforeFocusCallback,
+    WorkingData,
 } from '../../types';
+import { isWorkingData } from './isWorkingData';
 
 type MessageStore<T> = { [key in keyof T]: ValidationMessages };
 
@@ -14,9 +16,16 @@ interface Stores<T> {
     dataStore: ObservableMap<T>;
     messageStore: ObservableMap<MessageStore<T>>;
     state: ObservableMap<WorkingDataState>;
-    fields: Map<keyof T, Required<InternalFieldOptions<any, T>>>;
+    fields: Map<
+        keyof T,
+        Required<InternalFieldOptions<any, T>> | WorkingData<any>
+    >;
     refs: Map<keyof T, HTMLElement>;
-    validationFailedCallbacks: Map<keyof T, Set<ValidationFailedCallback>>;
+    children: Map<string, WorkingData<any>>;
+    validationFailedCallbacks: Map<
+        keyof T | '*',
+        Set<ValidationFailedCallback<T>>
+    >;
     beforeFocusCallbacks: Set<BeforeFocusCallback<keyof T>>;
 }
 
@@ -30,21 +39,28 @@ export const createStores = <T>(
     options: InternalWorkingDataOptions<T>,
 ): Stores<T> => {
     const initialValues: Record<string, any> = {};
+    const children = new Map<string, WorkingData<any>>();
     const messages: Record<string, any> = {};
     const fields = new Map<string, any>();
     const defaultState: WorkingDataState = {
         frozen: false,
     };
 
-    for (const [key, value] of Object.entries<InternalFieldOptions<any, any>>(
-        options,
-    )) {
-        initialValues[key] = value.initialValue;
-        messages[key] = blankMessages();
+    for (const [key, value] of Object.entries<
+        InternalFieldOptions<any, any> | WorkingData<any>
+    >(options)) {
         fields.set(key, value);
+
+        if (isWorkingData(value)) {
+            children.set(key, value);
+        } else {
+            initialValues[key] = value.initialValue;
+            messages[key] = blankMessages();
+        }
     }
 
     return {
+        children,
         dataStore: createStore<T>(initialValues as any),
         messageStore: createStore<MessageStore<T>>(messages as any),
         state: createStore<WorkingDataState>(defaultState),
