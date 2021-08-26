12017d77d4f07347dbd4095e7fbcd6b3055937c8
George Payne <georgepayne@hotmail.com>
2021-08-26 14:25:00 +0200

    @eventstore/stores documentation

--- packages/stores/src/stores/createListStore.ts
+++ packages/stores/src/stores/createListStore.ts
@@ -1,34 +1,52 @@
 import { $data } from '../symbols';
 import { createStore } from './createStore';
 
+/** A store for holding a list of objects. */
 export interface ListStore<T extends object> {
+    /** Reset the store to initial state. */
     reset: () => void;
+    /** The internal state of the store. */
     readonly state: Record<string, T>;
+    /** Delete an entry from the list. */
     delete: (id: string) => boolean;
+    /** Returns an array of own enumerable string-keyed property [key, value] pairs. (Similar to `Object.entries`) */
     entries: () => Array<[string, T]>;
+    /** Extract a list of ids from the store, with optional filter map. */
     extract: <R = T>(
         ids: string[],
         filterMap?: (item: T, i: number) => R | null,
     ) => Array<R>;
+    /** Get a single item from the list. */
     get: (id: string) => T | undefined;
+    /** Check if an item is available in the list. */
     has: (id: string) => boolean;
+    /** Returns an array of all the keys in the store. */
     keys: () => Array<string>;
+    /** Listen to a change on a single item in the store. */
     onChange(id: string, cb: (item: T) => void): () => void;
+    /** Listen to a change on the entire store. */
     onChange(cb: (list: Record<string, T>) => void): () => void;
+    /** Insert or overwrite value with key. */
     set: (id: string, item: T) => void;
+    /** Current number of items in the store. */
     size: number;
+    /** Update an item in the store. */
     update: (id: string, updater: (item: T) => T) => boolean;
+    /** Update an item in the store, waiting for it to be inserted. Will return false if the item wasnt inserted before maxWait. */
     updateWhenAvailable: (
         id: string,
         updater: (item: T) => T,
         maxWait?: number,
     ) => Promise<boolean>;
+    /** Update an item in the store, or set, if it isn't present. Returns false if the item was created, true if updated. */
     updateOrSet: (
         id: string,
         updater: (item: T) => T,
         creator: () => T,
     ) => boolean;
+    /** Returns an array of all the values in the store. */
     values: () => Array<T>;
+    /** Allows the store to be iterated over. */
     [Symbol.iterator]: () => Array<[string, T]>;
     /**
      * internal access to data
@@ -36,6 +54,7 @@ export interface ListStore<T extends object> {
     readonly [$data]: Readonly<Map<string, T>>;
 }
 
+/** Create a new list store with the given initial state. */
 export const createListStore = <T extends object>(
     initialState: Record<string, T> = {},
 ): ListStore<T> => {
