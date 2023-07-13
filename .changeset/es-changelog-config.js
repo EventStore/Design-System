59f44036dad70b84f4a507f49f44f65edc0cbfc1
teamchong <25894545+teamchong@users.noreply.github.com>
2023-07-13 00:07:03 -0400

    feat: setup changesets

---
+++ .changeset/es-changelog-config.js
@@ -0,0 +1,108 @@
+const { getInfo, getInfoFromPullRequest } = require("@changesets/get-github-info");
+
+require("dotenv").config();
+
+const changelogFunctions = {
+  getDependencyReleaseLine: async (
+    changesets,
+    dependenciesUpdated,
+    options
+  ) => {
+    if (!options.repo) {
+      throw new Error(
+        'Please provide a repo to this changelog generator like this:\n"changelog": ["./es-changelog-config.js", { "repo": "org/repo" }]'
+      );
+    }
+    if (dependenciesUpdated.length === 0) return "";
+
+    const changesetLink = `- Updated dependencies [${(
+      await Promise.all(
+        changesets.map(async (cs) => {
+          if (cs.commit) {
+            let { links } = await getInfo({
+              repo: options.repo,
+              commit: cs.commit,
+            });
+            return links.commit;
+          }
+        })
+      )
+    )
+      .filter((_) => _)
+      .join(", ")}]:`;
+
+    const updatedDepenenciesList = dependenciesUpdated.map(
+      (dependency) => `  - ${dependency.name}@${dependency.newVersion}`
+    );
+
+    return [changesetLink, ...updatedDepenenciesList].join("\n");
+  },
+  getReleaseLine: async (changeset, type, options) => {
+    if (!options || !options.repo) {
+      throw new Error(
+        'Please provide a repo to this changelog generator like this:\n"changelog": ["./es-changelog-config.js", { "repo": "org/repo" }]'
+      );
+    }
+
+    let prFromSummary;
+    let commitFromSummary;
+    let usersFromSummary = [];
+
+    const replacedChangelog = changeset.summary
+      .replace(/^\s*(?:pr|pull|pull\s+request):\s*#?(\d+)/im, (_, pr) => {
+        let num = Number(pr);
+        if (!isNaN(num)) prFromSummary = num;
+        return "";
+      })
+      .replace(/^\s*commit:\s*([^\s]+)/im, (_, commit) => {
+        commitFromSummary = commit;
+        return "";
+      })
+      .replace(/^\s*(?:author|user):\s*@?([^\s]+)/gim, (_, user) => {
+        usersFromSummary.push(user);
+        return "";
+      })
+      .trim();
+
+    const [firstLine, ...futureLines] = replacedChangelog
+      .split("\n")
+      .map((l) => l.trimEnd());
+
+    const links = await (async () => {
+      if (prFromSummary !== undefined) {
+        let { links } = await getInfoFromPullRequest({
+          repo: options.repo,
+          pull: prFromSummary,
+        });
+        if (commitFromSummary) {
+          links = {
+            ...links,
+            commit: `[\`${commitFromSummary}\`](https://github.com/${options.repo}/commit/${commitFromSummary})`,
+          };
+        }
+        return links;
+      }
+      const commitToFetchFrom = commitFromSummary || changeset.commit;
+      if (commitToFetchFrom) {
+        let { links } = await getInfo({
+          repo: options.repo,
+          commit: commitToFetchFrom,
+        });
+        return links;
+      }
+      return {
+        commit: null,
+        pull: null,
+        user: null,
+      };
+    })();
+
+    const prefix = links.commit === null ? "" : ` ${links.commit}`;
+
+    return `\n\n-${prefix ? `${prefix} -` : ""} ${firstLine}\n${futureLines
+      .map((l) => `  ${l}`)
+      .join("\n")}`;
+  },
+};
+
+module.exports = changelogFunctions;
--- readme.md
+++ readme.md
@@ -22,6 +22,22 @@ corepack enable
 yarn
 ```
 
+## Managing changelogs
+
+We use [changesets](https://github.com/changesets/changesets) to help us prepare releases. It helps us update package versions and add notes to the `CHANGELOG.md` for any package that's changed.
+
+A changeset is a record of changes made to specific packages. Create one when you need to publish a change that affects a package or the release notes. Not all changes need a changeset, though. For example, if your changes don't affect a package or the release notes, you don't need a changeset.
+
+### Creating a changeset
+
+1. Run `yarn changelog`
+2. Follow the prompts to describe your changes and pick the packages they affect. This selection helps determine which package versions need updates.
+3. This process generates a markdown changeset file in the `.changeset` directory. Each file outlines your changes and the affected packages.
+4. Include the changeset file(s) in your commit. They're essential for the upcoming release process.
+5. If necessary, you can generate multiple changesets by running the command multiple times. Each run generates a new changeset file.
+6. When pull request are merged into `main` branch, the changesets tool will automatically batch these changesets together and open a pull request with updates to the changelogs and version bumps for the affected packages.
+7. To prevent conflicts, it's recommended to merge this changeset pull request before merging more changes into the `main` branch. 
+
 ## Build
 
 Build packages in the repo:
