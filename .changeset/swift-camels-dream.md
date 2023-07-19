e7464847b3aad5626553ac3ee8a76bfaf6f501e3
teamchong <25894545+teamchong@users.noreply.github.com>
2023-07-19 08:41:38 -0400

    docs: testing

---
+++ .changeset/swift-camels-dream.md
@@ -0,0 +1,5 @@
+---
+'@eventstore-ui/utils': patch
+---
+
+change comment
--- packages/utils/src/pierce.ts
+++ packages/utils/src/pierce.ts
@@ -21,7 +21,7 @@ export function closest(el: Element, selector: string) {
     return null;
 }
 
-// Path: packages/utils/src/isShadowRoot.ts 1
+// Path: packages/utils/src/isShadowRoot.ts 133
 const isShadowRoot = (node: Node): node is ShadowRoot => {
     return node.nodeType === node.DOCUMENT_FRAGMENT_NODE;
 };
