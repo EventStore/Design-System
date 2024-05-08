f888953ed9f08329ef2a0eb6769b90f9b753a9c2
George Payne <georgepayne@hotmail.com>
2024-05-08 11:22:33 +0200

    Export panel helpers

--- packages/layout/src/index.ts
+++ packages/layout/src/index.ts
@@ -15,12 +15,12 @@ export type {
 } from './components/Page/Page';
 export type { PageViewProps } from './components/PageView/PageView';
 export type { LoadingBarStatus } from './components/es-loading-bar/types';
-export type * from './components/panel/types';
 export type { PageViewEvent, PageViewEventDetail } from './utils/pageView';
 
 export { ICON_NAMESPACE as ES_LAYOUT_ICON_NAMESPACE } from './icons/namespace';
 export { Page } from './components/Page/Page';
 export { PageView } from './components/PageView/PageView';
+export * from './components/panel';
 export { setProgress } from './components/es-loading-bar/utils/setProgress';
 export { dispatchPageViewEvent } from './utils/pageView';
 export * from './utils/LayoutVar';
