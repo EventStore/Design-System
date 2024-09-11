974cb5caa8b3d7fa763098c8762c3f276bd02acd
George Payne <georgepayne@hotmail.com>
2024-09-11 15:00:04 +0200

    Add number input and field

---
+++ packages/fields-v2/src/components/text/number/demos/number-input.demo.tsx
@@ -0,0 +1,50 @@
+import { Component, h, Host } from '@stencil/core';
+
+import { createValidatedForm } from '@eventstore-ui/forms';
+
+interface Example {
+    something: string;
+    option: boolean;
+    somethingElse: string;
+}
+
+/** number-input demo. */
+@Component({
+    tag: 'number-input-demo',
+    shadow: true,
+})
+export class Demo {
+    private form = createValidatedForm<Example>({
+        something: '',
+        option: false,
+        somethingElse: '',
+    });
+
+    render() {
+        return (
+            <Host style={{ padding: '10px', display: 'block' }}>
+                <form onSubmit={this.onSubmit}>
+                    <f2-number-input
+                        placeholder={'Something Else'}
+                        {...this.form.connect('somethingElse')}
+                    />
+                    <f2-number-input
+                        placeholder={'Something'}
+                        {...this.form.connect('something')}
+                    />
+                    <pre slot={'data'}>
+                        {JSON.stringify(this.form.data, null, 2)}
+                    </pre>
+                    <button>{'submit'}</button>
+                </form>
+            </Host>
+        );
+    }
+
+    private onSubmit = (e: Event) => {
+        e.preventDefault();
+
+        // eslint-disable-next-line no-console
+        console.log(new FormData(e.target as HTMLFormElement));
+    };
+}
---
+++ packages/fields-v2/src/components/text/number/number-input.tsx
@@ -0,0 +1,107 @@
+import {
+    Component,
+    h,
+    Prop,
+    Event,
+    type EventEmitter,
+    AttachInternals,
+    Host,
+} from '@stencil/core';
+import type { FieldChange } from 'types';
+
+/**
+ * A number based input.
+ * Values should be passed around as strings, as numbers can round / floating point / overflow etc if a number type is used.
+ */
+@Component({
+    tag: 'f2-number-input',
+    styleUrl: 'number.css',
+    formAssociated: true,
+    shadow: true,
+})
+export class NumberInput {
+    @AttachInternals() internals!: ElementInternals;
+
+    /** Emitted when the value of the field is changed. */
+    @Event({ bubbles: true }) fieldchange!: EventEmitter<FieldChange<string>>;
+    /** Emitted on keyup of enter, if no modifier keys are held. */
+    @Event() enter!: EventEmitter;
+
+    /** The name of the field. */
+    @Prop({ reflect: true }) name!: string;
+    /** The current value of the field. */
+    @Prop() value!: string;
+    /** The placeholder for the input. */
+    @Prop() placeholder?: string;
+    /** If the field is disabled. */
+    @Prop() disabled?: boolean;
+    /** If the field is editable. */
+    @Prop() readonly?: boolean;
+    /** If the field is currently in an error state. */
+    @Prop() invalid?: boolean;
+    /** Display a unit beside the input. */
+    @Prop() unit?: string;
+    /** Pass props directly to the input. */
+    @Prop() inputProps?: Record<string, any>;
+
+    componentDidLoad() {
+        this.internals.setFormValue(this.value);
+    }
+
+    render() {
+        return (
+            <Host>
+                <div class={'input_wrapper'}>
+                    <input
+                        {...(this.inputProps ?? {})}
+                        class={{ input: true, invalid: !!this.invalid }}
+                        part={'input'}
+                        type={'text'}
+                        inputmode={'numeric'}
+                        value={this.value}
+                        onInput={this.onInput}
+                        onKeyDown={this.onKeyDown}
+                        onKeyUp={this.onKeyUp}
+                        placeholder={this.placeholder}
+                        disabled={this.disabled}
+                        readonly={this.readonly}
+                    />
+                    {!!this.unit && (
+                        <span class={'unit'} part={'unit'}>
+                            {this.unit}
+                        </span>
+                    )}
+                </div>
+                <slot />
+            </Host>
+        );
+    }
+
+    private onKeyDown = (e: KeyboardEvent) => {
+        if (
+            e.ctrlKey ||
+            e.metaKey ||
+            e.key.length > 1 ||
+            /[0-9.]/.test(e.key)
+        ) {
+            return;
+        }
+
+        e.preventDefault();
+    };
+
+    private onKeyUp = (e: KeyboardEvent) => {
+        if (e.key === 'Enter') {
+            this.enter.emit();
+        }
+    };
+
+    private onInput = (e: Event) => {
+        const value = (e.target as HTMLInputElement).value ?? '';
+        this.internals.setFormValue(value);
+        this.fieldchange.emit({
+            name: this.name,
+            value,
+        });
+    };
+}
