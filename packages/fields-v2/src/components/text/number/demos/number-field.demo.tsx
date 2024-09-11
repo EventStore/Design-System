91ce18215e15915a7d432f07a3319345cc701fe3
George Payne <georgepayne@hotmail.com>
2024-09-11 15:00:04 +0200

    Add number input and field

---
+++ packages/fields-v2/src/components/text/number/demos/number-field.demo.tsx
@@ -0,0 +1,119 @@
+import { Component, h, Host } from '@stencil/core';
+
+import { createValidatedForm } from '@eventstore-ui/forms';
+import { ICON_NAMESPACE } from 'icons/namespace';
+
+interface Example {
+    something: string;
+    option: boolean;
+    somethingElse: string;
+}
+
+/** number-field demo. */
+@Component({
+    tag: 'number-field-demo',
+    shadow: true,
+})
+export class Demo {
+    private form = createValidatedForm<Example>({
+        something: '',
+        option: false,
+        somethingElse: '',
+    });
+
+    render() {
+        return (
+            <Host style={{ padding: '10px', display: 'block' }}>
+                <form onSubmit={this.onSubmit}>
+                    <f2-number-field
+                        label={'Something Else'}
+                        documentation={
+                            'This is where you put something important.'
+                        }
+                        documentationLink={
+                            'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+                        }
+                        placeholder={'Something Else'}
+                        {...this.form.connect('somethingElse')}
+                    />
+                    <f2-number-field
+                        label={'Something'}
+                        placeholder={'Something'}
+                        documentation={
+                            'This is not where you put something important.'
+                        }
+                        documentationLink={
+                            'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+                        }
+                        documentationLinkText={"Don't click on this"}
+                        unit={'kg'}
+                        {...this.form.connect('something')}
+                    />
+
+                    <f2-number-field
+                        label={'Something'}
+                        placeholder={'Something'}
+                        {...this.form.connect('something')}
+                    >
+                        <span
+                            slot={'documentation'}
+                            style={{
+                                display: 'flex',
+                                'align-items': 'center',
+                                gap: '8px',
+                                color: 'orange',
+                            }}
+                        >
+                            <es-icon
+                                size={14}
+                                icon={[ICON_NAMESPACE, 'trash']}
+                                style={{ color: 'pink' }}
+                            />
+                            {'I am some special docs'}
+                            <es-icon
+                                size={14}
+                                icon={[ICON_NAMESPACE, 'trash']}
+                                style={{ color: 'pink' }}
+                            />
+                        </span>
+                    </f2-number-field>
+
+                    <f2-number-field
+                        label={'Something'}
+                        placeholder={'Something'}
+                        documentation={
+                            'This is not where you put something important.'
+                        }
+                        documentationLink={
+                            'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+                        }
+                        documentationLinkText={"Don't click on this"}
+                        {...this.form.connect('something')}
+                        invalid
+                        messages={{
+                            error: ["Oh no! It's all wrogn!"],
+                            warning: ['You need to fix this...'],
+                            info: ['pls'],
+                        }}
+                    >
+                        <es-button>
+                            <es-icon icon={[ICON_NAMESPACE, 'trash']} />
+                        </es-button>
+                    </f2-number-field>
+
+                    <pre slot={'data'}>
+                        {JSON.stringify(this.form.data, null, 2)}
+                    </pre>
+                    <button>{'submit'}</button>
+                </form>
+            </Host>
+        );
+    }
+
+    private onSubmit = (e: Event) => {
+        e.preventDefault();
+
+        // eslint-disable-next-line no-console
+        console.log(new FormData(e.target as HTMLFormElement));
+    };
+}
---
+++ packages/fields-v2/src/components/text/number/readme.md
@@ -0,0 +1,127 @@
+# es-number-input
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Overview
+
+A number based input. Values should be passed around as strings, as numbers can round / floating point / overflow etc if a number type is used.
+
+## Usage
+
+### Example
+
+```tsx
+import { createValidatedForm } from '@eventstore-ui/forms';
+
+interface Example {
+    count: string;
+    mice: string;
+}
+
+const form = createValidatedForm<Example>({
+    count: '',
+    mice: {
+        initialValue: '',
+        validations: [
+            {
+                validator: (v) => parseInt(v, 10) >= 5,
+                message: 'There are at least 5 mice.',
+            },
+        ],
+    },
+});
+
+export default () => (
+    <>
+        <es-number-input
+            label={'Count'}
+            placeholder={'How high can you count'}
+            unit={'n'}
+            {...form.connect('count')}
+        />
+        <es-number-input
+            label={'How many mice?'}
+            placeholder={'Are there any?'}
+            unit={'🐁'}
+            {...form.connect('mice')}
+        />
+        <es-button
+            slot={'footer'}
+            onClick={() => {
+                form.submit((data) => {
+                    console.log(data);
+                });
+            }}
+        >
+            {'Submit'}
+        </es-button>
+    </>
+);
+```
+
+```css
+:host {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+es-button {
+    margin-left: auto;
+}
+```
+
+
+
+## Properties
+
+| Property             | Attribute     | Description                                  | Type                                 | Default     |
+| -------------------- | ------------- | -------------------------------------------- | ------------------------------------ | ----------- |
+| `disabled`           | `disabled`    | If the field is disabled.                    | `boolean \| undefined`               | `undefined` |
+| `inputProps`         | --            | Pass props directly to the input.            | `undefined \| { [x: string]: any; }` | `undefined` |
+| `invalid`            | `invalid`     | If the field is currently in an error state. | `boolean \| undefined`               | `undefined` |
+| `label` _(required)_ | `label`       | The label of the field.                      | `string`                             | `undefined` |
+| `messages`           | --            | The validation messages of the field         | `ValidationMessages \| undefined`    | `undefined` |
+| `name` _(required)_  | `name`        | The name of the field.                       | `string`                             | `undefined` |
+| `placeholder`        | `placeholder` | The placeholder for the input.               | `string \| undefined`                | `undefined` |
+| `readonly`           | `readonly`    | If the field is editable.                    | `boolean \| undefined`               | `undefined` |
+| `unit`               | `unit`        | Display a unit beside the input.             | `string \| undefined`                | `undefined` |
+| `value` _(required)_ | `value`       | The current value of the field.              | `string`                             | `undefined` |
+
+
+## Events
+
+| Event         | Description                                              | Type                               |
+| ------------- | -------------------------------------------------------- | ---------------------------------- |
+| `enter`       | Emitted on keyup of enter, if no modifier keys are held. | `CustomEvent<any>`                 |
+| `fieldchange` | Emitted when the value of the field is changed.          | `CustomEvent<FieldChange<string>>` |
+
+
+## CSS Custom Properties
+
+| Name                   | Description       |
+| ---------------------- | ----------------- |
+| `--field-grid-columns` | The field layout. |
+
+
+## Dependencies
+
+### Depends on
+
+- [es-validation-messages](../es-validation-messages)
+
+### Graph
+```mermaid
+graph TD;
+  es-number-input --> es-validation-messages
+  es-validation-messages --> es-icon
+  style es-number-input fill:#f9f,stroke:#333,stroke-width:4px
+```
+
+----------------------------------------------
+
+
