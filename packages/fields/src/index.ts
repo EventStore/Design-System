341939d882aa8d189a1977606c4977cb1b56e6dc
George Payne <georgepayne@hotmail.com>
2024-09-23 13:39:19 +0200

    re-export types

--- packages/fields/src/index.ts
+++ packages/fields/src/index.ts
@@ -1,11 +1,31 @@
 export type { Components, JSX } from './components';
 
+export type { MaskOptions } from './components/masked-text/types';
+export type {
+    RadioCardOption,
+    RenderCard,
+} from './components/radio-card/types';
+export type { MultiCheckboxOption } from './components/multi-checkbox/types';
+export type {
+    RenderSelectValue,
+    RenderSelectOption,
+} from './components/select/types';
+export type {
+    OptionFilter,
+    RenderTypeaheadInput,
+    RenderTypeaheadOption,
+    TypeaheadOption,
+    RenderTypeaheadFieldParams,
+} from './components/typeahead/types';
+export type { FieldProps } from './components/Field';
+
 export type {
     Severity,
     ValidationMessage,
     ValidationMessages,
     FieldChange,
     FieldChangeEvent,
+    Templated,
 } from './types';
 
 export { ICON_NAMESPACE as ES_FIELDS_ICON_NAMESPACE } from './icons/namespace';
