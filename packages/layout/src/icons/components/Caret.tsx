0709ef5b4c6a764d9a529d9054c43625140645d8
George Payne <georgepayne@hotmail.com>
2024-11-25 15:04:30 +0100

    update es-layout to new icons

--- packages/layout/src/icons/components/Caret.tsx
+++ packages/layout/src/icons/components/Caret.tsx
@@ -3,6 +3,6 @@ import type { h as JSXFactory } from '@stencil/core';
 
 export const Caret = (h: typeof JSXFactory) => (props: any) => (
     <svg {...props} viewBox="0 0 24 24" aria-hidden="true">
-        <path d="M5.9 9H18c.8 0 1.3 1 .7 1.6l-6.1 6.1c-.4.4-.9.4-1.3 0l-6.1-6.1C4.7 10 5.1 9 5.9 9z" />
+        <path d="m7 10 5 5 5-5H7Z" />
     </svg>
 );
