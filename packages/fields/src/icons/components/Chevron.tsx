a79fb4f779754b8ec10939a2acff72025e472059
George Payne <georgepayne@hotmail.com>
2024-11-25 14:59:14 +0100

    update es-fields to new icons

--- packages/fields/src/icons/components/Chevron.tsx
+++ packages/fields/src/icons/components/Chevron.tsx
@@ -2,14 +2,7 @@
 import type { h as JSXFactory } from '@stencil/core';
 
 export const Chevron = (h: typeof JSXFactory) => (props: any) => (
-    <svg
-        {...props}
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        stroke="currentColor"
-        aria-hidden="true"
-        viewBox="0 0 24 24"
-    >
-        <path d="m18.5 9-6 6-6-6" />
+    <svg {...props} viewBox="0 0 24 24" aria-hidden="true">
+        <path d="M7.41 8.58 12 13.17l4.59-4.59L18 10l-6 6-6-6 1.41-1.42Z" />
     </svg>
 );
