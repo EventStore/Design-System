6ec701adb4d4dc294ddcc5ec6133377c51e8a065
George Payne <georgepayne@hotmail.com>
2024-11-25 14:59:14 +0100

    update es-fields to new icons

--- packages/fields/src/icons/components/Edit.tsx
+++ packages/fields/src/icons/components/Edit.tsx
@@ -2,13 +2,7 @@
 import type { h as JSXFactory } from '@stencil/core';
 
 export const Edit = (h: typeof JSXFactory) => (props: any) => (
-    <svg
-        {...props}
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-        aria-hidden="true"
-    >
-        <path d="M3.5 21h18M5.5 13.36V17h3.659L19.5 6.654 15.848 3 5.5 13.36Z" />
+    <svg {...props} viewBox="0 0 24 24" aria-hidden="true">
+        <path d="M20.71 7.04c.39-.39.39-1.04 0-1.41l-2.34-2.34c-.37-.39-1.02-.39-1.41 0l-1.84 1.83 3.75 3.75M3 17.25V21h3.75L17.81 9.93l-3.75-3.75L3 17.25Z" />
     </svg>
 );
