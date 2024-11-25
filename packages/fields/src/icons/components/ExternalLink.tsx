69e8060955dd418c5c69f989258fea31aa0e1aea
George Payne <georgepayne@hotmail.com>
2024-11-25 14:59:14 +0100

    update es-fields to new icons

--- packages/fields/src/icons/components/ExternalLink.tsx
+++ packages/fields/src/icons/components/ExternalLink.tsx
@@ -2,7 +2,7 @@
 import type { h as JSXFactory } from '@stencil/core';
 
 export const ExternalLink = (h: typeof JSXFactory) => (props: any) => (
-    <svg {...props} aria-hidden="true" viewBox="0 0 512 512">
-        <path d="M440 256h-16a8 8 0 00-8 8v200a16 16 0 01-16 16H48a16 16 0 01-16-16V112a16 16 0 0116-16h200a8 8 0 008-8V72a8 8 0 00-8-8H48a48 48 0 00-48 48v352a48 48 0 0048 48h352a48 48 0 0048-48V264a8 8 0 00-8-8zM500 0L364 .34a12 12 0 00-12 12v10a12 12 0 0012 12l90-.34.7.71-323.19 323.15a12 12 0 000 17l5.66 5.66a12 12 0 0017 0L477.29 57.34l.71.7-.34 90a12 12 0 0012 12h10a12 12 0 0012-12L512 12a12 12 0 00-12-12z" />
+    <svg {...props} viewBox="0 0 24 24" aria-hidden="true">
+        <path d="M14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3m-2 16H5V5h7V3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7h-2v7Z" />
     </svg>
 );
--- packages/fields/src/icons/components/Warning.tsx
+++ packages/fields/src/icons/components/Warning.tsx
@@ -3,11 +3,6 @@ import type { h as JSXFactory } from '@stencil/core';
 
 export const Warning = (h: typeof JSXFactory) => (props: any) => (
     <svg {...props} viewBox="0 0 24 24" aria-hidden="true">
-        <path
-            fill="none"
-            stroke="currentColor"
-            d="M12 22c2.7 0 5.2-1 7.1-2.9S22 14.7 22 12s-1-5.2-2.9-7.1S14.7 2 12 2 6.8 3 4.9 4.9 2 9.3 2 12s1 5.2 2.9 7.1C6.8 21 9.3 22 12 22z"
-        />
-        <path d="M10.4 5.3h3.1l-.3 9.2h-2.6l-.2-9.2zm.2 10.6h2.7v2.8h-2.7v-2.8z" />
+        <path d="M13 13h-2V7h2m0 10h-2v-2h2M12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10 10 10 0 0 0 10-10A10 10 0 0 0 12 2Z" />
     </svg>
 );
