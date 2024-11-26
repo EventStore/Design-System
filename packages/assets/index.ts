f4c13048762ea210e8d0e4ae4f251b52ca81473e
George Payne <georgepayne@hotmail.com>
2024-11-26 12:29:54 +0100

    Replace fonts with new brand fonts - Add font files - Add css files - Add more export paths for more fine-grained control of inclusions - Update fonts in layout root.css

--- packages/assets/index.ts
+++ packages/assets/index.ts
@@ -1,10 +1,33 @@
 import { join, dirname } from 'path';
 
+const root = dirname(require.resolve('@eventstore-ui/assets/package.json'));
+
 /**
- * Path to the location of the assets
- * Include the assets in your stencil project by adding them to "copy" in your stencil config.
+ * Path to the location of all assets
+ * Include in your stencil project by adding the path to "copy" in your stencil config.
  */
-export const assetsPath = join(
-    dirname(require.resolve('@eventstore-ui/assets/package.json')),
-    'assets',
-);
+export const assetsPath = join(root, 'assets');
+
+/**
+ * Path to the location of the favicons
+ * Include in your stencil project by adding the path to "copy" in your stencil config.
+ */
+export const faviconsPath = join(assetsPath, 'favicons');
+
+/**
+ * Path to the location of all fonts
+ * Include in your stencil project by adding the path to "copy" in your stencil config.
+ */
+export const fontsPath = join(assetsPath, 'fonts');
+
+/**
+ * Path to the location of "Spline Sans Mono" font
+ * Include in your stencil project by adding the path to "copy" in your stencil config.
+ */
+export const splineSansMonoPath = join(fontsPath, 'assets');
+
+/**
+ * Path to the location of the "Work Sans" font
+ * Include in your stencil project by adding the path to "copy" in your stencil config.
+ */
+export const workSansPath = join(fontsPath, 'assets');
