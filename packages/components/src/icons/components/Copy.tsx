8d369b43713fcc7a366cf6c8587a75b48c5303b2
George Payne <georgepayne@hotmail.com>
2024-11-27 18:05:15 +0100

    revert icon changes

--- packages/components/src/icons/components/Copy.tsx
+++ packages/components/src/icons/components/Copy.tsx
@@ -2,7 +2,15 @@
 import type { h as JSXFactory } from '@stencil/core';
 
 export const Copy = (h: typeof JSXFactory) => (props: any) => (
-    <svg {...props} viewBox="0 0 24 24" aria-hidden="true">
-        <path d="M19 21H8V7h11m0-2H8a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2m-3-4H4a2 2 0 0 0-2 2v14h2V3h12V1Z" />
+    <svg
+        {...props}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        stroke="currentColor"
+        aria-hidden="true"
+        viewBox="0 0 24 24"
+    >
+        <path d="M6.5 6.216v-2.31c0-.776.63-1.406 1.406-1.406h12.188c.776 0 1.406.63 1.406 1.406v12.188c0 .776-.63 1.406-1.406 1.406h-2.336" />
+        <path d="M16.094 6.5H3.906C3.13 6.5 2.5 7.13 2.5 7.906v12.188c0 .776.63 1.406 1.406 1.406h12.188c.776 0 1.406-.63 1.406-1.406V7.906c0-.776-.63-1.406-1.406-1.406Z" />
     </svg>
 );
