9e502402859088983e94b1430b06573e3d5efa16
George Payne <georgepayne@hotmail.com>
2024-11-27 18:05:15 +0100

    revert icon changes

--- packages/fields/src/icons/components/Trash.tsx
+++ packages/fields/src/icons/components/Trash.tsx
@@ -2,7 +2,14 @@
 import type { h as JSXFactory } from '@stencil/core';
 
 export const Trash = (h: typeof JSXFactory) => (props: any) => (
-    <svg {...props} viewBox="0 0 24 24" aria-hidden="true">
-        <path d="M19 4h-3.5l-1-1h-5l-1 1H5v2h14M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6v12Z" />
+    <svg
+        {...props}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        stroke="currentColor"
+        aria-hidden="true"
+        viewBox="0 0 24 24"
+    >
+        <path d="M4.5 5v17h15V5h-15ZM10 10v6.5M14 10v6.5M2 5h20M8 5l1.645-3h4.744L16 5H8Z" />
     </svg>
 );
