630995beff2d24198218b5b8cb44bc0601a46f7f
George Payne <georgepayne@hotmail.com>
2025-01-15 16:46:15 +0100

    rename branchIsActive to shouldValidateBranch

--- packages/forms/src/symbols.ts
+++ packages/forms/src/symbols.ts
@@ -2,4 +2,4 @@ export const wDKey = Symbol('formdata');
 export const triggerValidation = Symbol('triggerValidation');
 export const focusError = Symbol('focusError');
 export const insertError = Symbol('insertError');
-export const branchIsActive = Symbol('branchIsActive');
+export const shouldValidateBranch = Symbol('shouldValidateBranch');
