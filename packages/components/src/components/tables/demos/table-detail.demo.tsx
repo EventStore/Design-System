5180efd8d87ec7f6cf5406f6450d52437ea23cff
George Payne <georgepayne@hotmail.com>
2025-02-06 17:59:34 +0100

    Improve table details
    Table details has been redesigned to be
    
    -   More information dense
    -   Handle longer labels and data
    -   Fluidly size itself to it's available space
    
    Additionally, cell auto extract now handles more data types by attempting to convert all data type to a string, otherwise returning an empty placeholder.

--- packages/components/src/components/tables/demos/table-detail.demo.tsx
+++ packages/components/src/components/tables/demos/table-detail.demo.tsx
@@ -6,6 +6,22 @@ interface DummyData {
     name: string;
     value: string;
     amount: number;
+
+    eventSource: string;
+    groupName?: string;
+    resolveLinkTos: boolean;
+    startFrom: string;
+    messageTimeoutMilliseconds: number;
+    extraStatistics: boolean;
+    maxRetryCount: number;
+    liveBufferSize: number;
+    bufferSize: number;
+    readBatchSize: number;
+    checkPointAfterMilliseconds: number;
+    minCheckPointCount: number;
+    maxCheckPointCount: number;
+    maxSubscriberCount: number;
+    namedConsumerStrategy: string;
 }
 
 /**
@@ -22,6 +38,22 @@ export class TableDetailDemo {
         name: 'test',
         value: 'something here',
         amount: 12,
+        eventSource: 'my_stream',
+        groupName:
+            'my_group_has_a_very_long_name_so_I_have_been_set_to_full_width',
+        resolveLinkTos: true,
+        startFrom: '',
+        messageTimeoutMilliseconds: 1000,
+        extraStatistics: true,
+        maxRetryCount: 12,
+        liveBufferSize: 200,
+        bufferSize: 10,
+        readBatchSize: 20,
+        checkPointAfterMilliseconds: 1000,
+        minCheckPointCount: 5,
+        maxCheckPointCount: 20,
+        maxSubscriberCount: 100,
+        namedConsumerStrategy: 'RoundRobin',
     };
 
     render() {
@@ -57,6 +89,55 @@ export class TableDetailDemo {
                 large: amount >= 10,
             }),
         },
+
+        eventSource: {
+            title: 'Event Source',
+        },
+        groupName: {
+            title: 'Group Name',
+            variant: 'full-width',
+        },
+        resolveLinkTos: {
+            title: 'Resolve Links',
+        },
+        startFrom: {
+            title: 'Start From',
+        },
+        messageTimeoutMilliseconds: {
+            title: 'Message Timeout',
+        },
+        extraStatistics: {
+            title: 'Extra Statistics',
+        },
+        maxRetryCount: {
+            title: 'Max Retry Count',
+        },
+        liveBufferSize: {
+            title: 'Live Buffer Size',
+        },
+        bufferSize: {
+            title: 'Buffer Size',
+        },
+        readBatchSize: {
+            title: 'Read Batch Size',
+        },
+        checkPointAfterMilliseconds: {
+            title: 'Checkpoint After',
+            cell: (_, { data }) => `${data.checkPointAfterMilliseconds}ms`,
+        },
+        minCheckPointCount: {
+            title: 'Min Checkpoint Count',
+        },
+        maxCheckPointCount: {
+            title: 'Max Checkpoint Count',
+        },
+        maxSubscriberCount: {
+            title: 'Max Subscriber Count',
+        },
+        namedConsumerStrategy: {
+            title: 'Named Consumer Strategy',
+        },
+
         actions: {
             title: 'Amount',
             cell: (h) => <c2-button>{'Hello'}</c2-button>,
--- packages/components/src/components/tables/utils/autoExtract.ts
+++
@@ -1,8 +0,0 @@
-export const autoExtract = (data: any, name: string) => {
-    const value = data?.[name];
-    return typeof value === 'string' ||
-        typeof value === 'bigint' ||
-        typeof value === 'number'
-        ? value
-        : null;
-};
